01d3d6abe017b9bad73f0712f9a92f458467c8e5
Iyion <djaros96@yahoo.de>
2019-07-07 15:43:11 +0200

    Ersetzen der alten Dateien

--- ui.R
+++ ui.R
@@ -1,18 +1,3 @@
 ui <- fluidPage(
-  titlePanel("Hallo Welt!"),
-  
-  sidebarLayout(
-    position = "right",
-    sidebarPanel("Sidebar Panel"),
-    mainPanel(
-      h1("Main Panel",align="center"), br(),
-      a(href="https://www.google.de","www.google.de",Color="FF0000"), br(),
-      div(
-        em("Dies ist ein Textabschnitt"),
-        style="color:blue"), br(),
-      code(
-        "if a = b"), br(),
-      img(src = "Toffifee.jpg", height=500, width=500)
-    )
-  )
+
 )
