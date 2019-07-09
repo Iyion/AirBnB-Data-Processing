7bd20951e7f1c83ca25d52ad07ac198392a8155c
Iyion <djaros96@yahoo.de>
2019-07-09 19:51:28 +0200

    Added test functionality to filter after quarter

--- server.R
+++ server.R
@@ -1,8 +1,21 @@
+library(dplyr)
+
 server <- function(input, output) {
+  
+  ## Reactive elements
+
+  data_sub <- reactive({
+    filter(data, data$neighbourhood_group %in% input$checkQuarter)
+  })
+  
+  output$testtxt <- renderText({
+    paste("Du hast folgende Bezirke gewählt:", data_sub())
+  })
   
   # Render Map with the data points
   output$kartenoutput <- renderLeaflet({
-    leaflet()%>%addTiles()%>%addCircles(data$longitude, data$latitude, color=input$radioColors)
+    data_sub <- select_all(data_sub())
+    leaflet()%>%addTiles()%>%addCircles(data_sub$longitude, data_sub()$latitude, color=input$radioColors)
     
   })
 }
--- ui.R
+++ ui.R
@@ -7,27 +7,49 @@ ui <- fluidPage(
   style = "background-color: rgba(200, 200, 200, 0.3",
   
   titlePanel("AirBnB-Daten in Berlin"),
-  
-  mainPanel(
-    tabsetPanel(
-      tabPanel("Karte", 
-               sidebarPanel(
-                 radioButtons("radioColors", h3("Farbe der Ausgabewerte"),
-                              choices = list("Blau" = "blue",
-                                             "Rot" = "red",
-                                             "Grün" = "green"),
-                              selected = "blue"),
-                 numericInput("num",
-                              h3("Numeric Input"),
-                              value = 0)),
-                 actionButton("rowAnimation", "Starte Animation")
-                 
-                 ),
-               mainPanel(
+ 
+  tabsetPanel(
+    tabPanel("Karte",
+             sidebarPanel(
+               radioButtons("radioColors", h3("Farbe der Ausgabewerte"),
+                            choices = list("Blau" = "blue",
+                                           "Rot" = "red",
+                                           "Grün" = "green"),
+                            selected = "blue"),
+               fluidPage(
+                 checkboxGroupInput("checkQuarter",
+                                    h3("Filtere nach Bezirken"),
+                                    choices = list("Friedrichshain - Kreuzberg",
+                                                   "Mitte",
+                                                   "Neukölln",
+                                                   "Tempelhof - Schöneberg",
+                                                   "Lichtenberg",
+                                                   "Charlottenburg-Wilm.",
+                                                   "Marzahn - Hellersdorf",
+                                                   "Pankow",
+                                                   "Steglitz - Zehlendorf",
+                                                   "Reinickendorf",
+                                                   "Spandau",
+                                                   "Treptow - Köpenick"),
+                                    selected = list()),
+                 actionButton("selectAll", "Alle markieren"),
+                 actionButton("deselectAll", "Alle demarkieren"),
+                 textOutput("testtxt")
+                 )
+               ),
+             
+             
+             mainPanel(
                leafletOutput("kartenoutput"))
-      ),
-      tabPanel("Auswertungen", "Auswertungen"),
-      
-      tabPanel("Rohdaten", "Rohdaten")
-  )
+    ),
+    
+    
+    tabPanel("Auswertungen", "Auswertungen"),
+    
+    tabPanel("Rohdaten", "Rohdaten")),
+    
+  
+
+  
+  mainPanel()
 )
